db07c96b35723d37a0d5398e75009f551eaa80a4
oceanss7 <1923777071@qq.com>
2023-03-07 08:57:25 +0800

    对于firstcode的首次提交

---
+++ firstcode.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<string.h>
+int  main(void)
+{
+   printf("first output msg...\n");
+   while(1)
+   {
+              sleep(1);
+	      return 0;
+   }
+   
+}
+
